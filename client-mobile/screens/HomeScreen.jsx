fe8268254268b918baf1ca833ed980db230dc222
petersatria <180710018@students.uajy.ac.id>
2023-06-25 14:46:26 +0700

    feat: card items and detail item

--- client-mobile/screens/HomeScreen.jsx
+++ client-mobile/screens/HomeScreen.jsx
@@ -8,6 +8,7 @@ import {
   Image,
   ScrollView,
   FlatList,
+  TouchableWithoutFeedback,
 } from "react-native";
 
 import { useQuery, gql } from "@apollo/client";
@@ -23,54 +24,37 @@ const GET_ITEMS = gql`
   }
 `;
 
+import Card from "../components/Card";
+
 export default function HomeScreen({ navigation }) {
   const { loading, error, data } = useQuery(GET_ITEMS);
-  // console.log(error);
 
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error : {error.message}</Text>;
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View className="bg-white flex-1">
-        <Image
-          className="w-full h-36"
-          source={{
-            uri: "https://img.freepik.com/free-photo/classic-luxury-style-restaurant-with-tables-chairs_140725-9389.jpg?w=900&t=st=1687256728~exp=1687257328~hmac=c2ed26a29cd48d7d1c072c09908534ad48a403662ed475c04b295a6d474414dc",
+    <View className="bg-white flex-1">
+      <Image
+        className="w-full h-36"
+        source={{
+          uri: "https://img.freepik.com/free-photo/classic-luxury-style-restaurant-with-tables-chairs_140725-9389.jpg?w=900&t=st=1687256728~exp=1687257328~hmac=c2ed26a29cd48d7d1c072c09908534ad48a403662ed475c04b295a6d474414dc",
+        }}
+      />
+      <View className="flex-1 items-center justify-center bg-primary-yellow">
+        <FlatList
+          className="my-5"
+          data={data.findItems}
+          renderItem={({ item }) => {
+            return (
+              <Pressable
+                onPress={() => navigation.navigate("Detail", { id: item.id })}
+              >
+                <Card item={item} />
+              </Pressable>
+            );
           }}
         />
-        <View className="flex-1 items-center justify-center bg-red-500">
-          {/* <Image
-            className="w-full h-48 object-contain"
-            source={require("../assets/logo.png")}
-          /> */}
-          {/* <Text className="text-primary-yellow">Home Screen</Text> */}
-          {/* <Pressable
-            className="bg-primary-yellow py-2 px-3 rounded-md focus:bg-yellow-600"
-            onPress={() => {
-              navigation.navigate("Menu");
-            }}
-          >
-            <Text className="text-white">Our Menu</Text>
-          </Pressable> */}
-          <FlatList
-            data={data.findItems}
-            renderItem={({ item }) => {
-              return (
-                <View className="px-2 py-3 bg-white my-2 rounded-md shadow-md flex">
-                  {/* <Image
-                    className="w-full h-36"
-                    source={{
-                      uri: `${item.imgUrl}`,
-                    }}
-                  /> */}
-                  <Text className="text-cyan-800">{item.name}</Text>
-                </View>
-              );
-            }}
-          />
-        </View>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
